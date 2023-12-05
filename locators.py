39c0e1c112bab418fb504027e323b432ba7ee7b2
Maksim Brykov <morfin1304@yandex.ru>
2023-12-05 05:46:58 +0300

    add_new_file

--- locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-BUTTON_LOGIN = By.XPATH, "//button[text()=['Войти в аккаунт']"
-INPUT_EMAIL = By.XPATH, "//*[@name='name']"
-INPUT_PASSWORD = By.XPATH, "//*[type='Пароль']"
--- tests/test1.py
+++ tests/test1.py
@@ -1,16 +1,20 @@
-import time
-import locators
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
+import conftest
+from conftest import login
+BUTTON_LOGIN = By.XPATH, "//button[text()=['Войти в аккаунт']"
+INPUT_EMAIL = By.XPATH, "//*[@name='name']"
+INPUT_PASSWORD = By.XPATH, "//*[type='Пароль']"
+
 class TestStellarBurfers:
     def test_mane_page(self):
         driver = webdriver.Chrome()
+        driver.set_window_size(1920, 1080)
         driver.get("https://stellarburgers.nomoreparties.site/")
-        driver.find_element()
-        driver.close()
+        driver.find_element(BUTTON_LOGIN).click()
+        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located((INPUT_EMAIL)))
         assert 1 == 1
 
